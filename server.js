910d3c0ffc251c71657ec409cdc6c423eeb2696a
vagrant <vagrant@kvm-host>
2026-02-24 03:05:24 +0000

    Update to real web server

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-// 'public' 폴더 안에 있는 정적 파일(HTML, CSS, 이미지 등)을 서비스하겠다고 선언
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(port, () => {
- console.log( Express 웹 서버가 ${port}번 포트에서 가동 중입니다...);
+    console.log(`Express 웹 서버가 ${port}번 포트에서 가동 중입니다...`);
 });
 
